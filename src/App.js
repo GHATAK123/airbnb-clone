01da12a0acae03d3be6bbb8145e12dfeaabe9772
Prakash Anand <ppa58514@gmail.com>
2020-08-27 05:01:33 +0530

    Airbnb-clone Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import Header from './Header'
-import './App.css';
-import Home from './Home';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import "./App.css";
+import Home from "./Home";
+import Footer from "./Footer";
+import SearchPage from "./SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-     <Header />
-     <Home />
-     <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
