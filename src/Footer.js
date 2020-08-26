3eb58ca9ca2b11c354caacf4567e4d1e9aa455e2
Prakash Anand <ppa58514@gmail.com>
2020-08-27 04:15:15 +0530

    Footer Done

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2020 Airbnb clone! No rights reserved - Prakash Anand<a href="https://github.com/GHATAK123/airbnb-clone"><b>(Ghatak123)</b></a></p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
